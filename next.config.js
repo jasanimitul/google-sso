bf343866565815cb2375ef17fc0f52e24e0379c2
Mitul jasani <jasanimitul@gmail.com>
2023-09-23 15:42:12 +0530

    Fix env

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_SECRET: process.env.NEXT_PUBLIC_SECRET,
 
     // Contentful
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
